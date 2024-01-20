603e2f1983cb1a4d4fc8611e3a8d6991e30228e9
ravindudinushan <ravindudinushan9966@gmail.com>
2024-01-20 16:45:28 +0530

    first commit

---
+++ BackEnd-POS/src/main/java/lk/ijse/gdse66/HelloServlet.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+}
