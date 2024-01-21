f4a419a951760b49b95dfa8bcde76a55fb0558a2
ravindudinushan <ravindudinushan9966@gmail.com>
2024-01-21 22:09:53 +0530

    pacageing

---
+++ BackEnd-POS/src/main/java/lk/ijse/gdse66/util/CrudUtil.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse66.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T> T execute(Connection connection ,String sql,Object...params) throws SQLException, ClassNotFoundException {
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            statement.setObject((i + 1), params[i]);
+        }
+        if (sql.startsWith("SELECT")) {
+            return (T) statement.executeQuery();
+        } else {
+            return (T) (Boolean) (statement.executeUpdate() > 0);
+        }
+    }
+}
+
