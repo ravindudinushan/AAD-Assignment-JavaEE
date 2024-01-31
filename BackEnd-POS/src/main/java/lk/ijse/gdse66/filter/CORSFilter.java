29a9e3c7055064baec8e6be59b78709041602de1
ravindudinushan <ravindudinushan9966@gmail.com>
2024-01-31 17:07:46 +0530

    front-end

--- BackEnd-POS/src/main/java/lk/ijse/gdse66/filter/CORSFilter.java
+++ BackEnd-POS/src/main/java/lk/ijse/gdse66/filter/CORSFilter.java
@@ -2,28 +2,24 @@ package lk.ijse.gdse66.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/*")
-public class CORSFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
+@WebFilter(filterName = "CORSFilter", value = "/*")
+public class CORSFilter extends HttpFilter {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        filterChain.doFilter(servletRequest, servletResponse);
-        HttpServletResponse resp = (HttpServletResponse) servletResponse;
-        resp.addHeader("Access-Control-Allow-Origin", "*");
-        resp.addHeader("Content-Type", "application/json");
-        resp.addHeader("Access-Control-Allow-Methods", "DELETE, PUT");
-        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
-    }
-
-    @Override
-    public void destroy() {
-
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String origin = req.getHeader("origin");
+        if (origin == null){
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST,"CORS Policy Violation");
+            return;
+        }
+        res.addHeader("Access-Control-Allow-Origin",origin);
+        res.addHeader("Access-Control-Allow-Headers","Content-Type");
+        res.addHeader("Access-Control-Allow-Methods","DELETE,PUT,GET,POST,OPTION,HEAD");
+        chain.doFilter(req,res);
     }
 }
